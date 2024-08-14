efb13b3685773676706e7934b3fb52dc260b2f57
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-08-14 12:51:14 +0300

    Addition of category

---
+++ src/components/Category/CategoryPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+import "./Category.css";
+import ProductList from "../ProductList/ProductList";
+
+function CategoryPage() {
+  return (
+    <div>
+      <div>
+        <h1>Category</h1>
+      </div>
+
+      <div className="ProductList">
+        <ProductList />
+      </div>
+    </div>
+  );
+}
+
+export default CategoryPage;
