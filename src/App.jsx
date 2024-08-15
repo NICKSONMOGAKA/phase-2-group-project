87e703d371ab19f8efe11d5d26c6a7a80c9b24d9
+ <nickson.mogaka@student.moringaschool.com>
2024-08-15 08:21:54 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import Footer from './Footer'
-import './App.css'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
-  
-
   return (
-    <>
-      
-        <div>
-        <Footer/>
-        </div>
-        
-      
-     
-    </>
-  )
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
